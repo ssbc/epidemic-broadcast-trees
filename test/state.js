81ba49ac246ece3e19839f03417850e0785365f1
Dominic Tarr <dominic.tarr@gmail.com>
2017-03-23 19:24:27 +1300

    tidy

--- test/state.js
+++ test/state.js
@@ -21,49 +21,6 @@ tape('receiveMessage 1', function (t) {
 //we can also be sending, receiving, idle (neither), or both.
 //but if both, normally one will ask to stop receiving soon.
 
-/*
-  sending:
-    +n+1
-      they are ahead, ask to receive.
-      this might happen on initial connections, if they think you might be further ahead.
-      they have asked us to send, but they'll probably change their mind. +send +receive
-    +n
-      we are in sync, and neither knows who is closer to the source.
-      since the actual receiver does not. +send
-    +n-1
-      they are behind us, get the next item for them. +send !get
-
-    -(n+1)
-      they are ahead of us, stop sending.
-      ask to receive from them (but they don't want anything from us) -send +receive
-    -n
-      stop sending, we know they are in sync, so no need to send notes. -send
-    -(n-1)
-      stop sending, but send notes sometimes. -send
-
-  receiving:
-    +n+1
-      they ask to receive from us, but we are not up to there.
-      *enable send* I guess, but don't expect to send. +send
-    +n
-      they ask to receive from us, but we are already in sync.
-      send a note to let the know we are in sync, if we havn't already.
-    +(n-1)
-      they ask to receive, but are behind us. +send
-
-    -(n+1)
-      expect them still to send to us, though. -send
-    -(n)
-      they just let us know we are in sync
-    -(n-1)
-      they are behind us, but are still getting messages faster than they would from us.
-      get ready to send a note about where we are up to.
-
-  if the absolute value is greater, and we are not already receiving, ask to receive.
-  else if positive, turn on send (and get ready if we have something)
-  else if negative, turn off send.
-*/
-
 tape('receiveNote, remote requests for sequence we have', function (t) {
 
   var local = {alice: 2}
@@ -135,9 +92,11 @@ tape('receiveNote, remote requests unrequests message when we are not sending', 
 
 //things that should be true, after receiving a note.
 function post_receiveNote (_state, state, note) {
+  var t = this
+  t.notOk(_state.effect)
   //if negative turn off send
   var seq = state.local[note.id]
-
+  
   if(note.seq < 0) {
     t.equal(state.sending, false, 'disable sending mode')
   }
@@ -145,7 +104,8 @@ function post_receiveNote (_state, state, note) {
     //if positive, turn on send
     t.equal(state.sending, true, 'enable sending')
     //if behind us, get ready to send
-    //what if we where ALREADY get'ing something?
+    //we should never see an effect set on _state.
+
     if(seq > note.seq)
       t.deepEqual(state.effect, {action: 'get', value: {id: note.id, seq: note.seq + 1}}, 'retrive next message to send')
   }
@@ -186,10 +146,11 @@ tape('receiveNote, with random seqs and signs', function (t) {
       local: local, sending: Math.random()<0.5, receiving: Math.random()<0.5,
       ready: Math.random() < 0.2 ? {author: 'alice', sequence: 5, content: 'hello'} : null
     }
+
     var _state = JSON.parse(JSON.stringify(state))
     var note = {id: 'alice', seq: ~~(Math.random()*20) - 10}
     state = states.receiveNote(state, note)
-    post_receiveNote(_state, state, note)
+    post_receiveNote.call(t, _state, state, note)
   }
 
   //What if something was ALREADY in ready, and a note is received?
@@ -198,11 +159,6 @@ tape('receiveNote, with random seqs and signs', function (t) {
 
   t.end()
 })
-
-
-
-
-
 
 
 
