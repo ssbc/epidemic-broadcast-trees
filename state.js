25d3ffce976da5942c6edb1c58b040bde4ab6a60
Dominic Tarr <dominic.tarr@gmail.com>
2017-03-23 22:34:10 +1300

    handle receiveMessage

--- state.js
+++ state.js
@@ -30,23 +30,23 @@ exports.read = function (state) {
 
 exports.receiveMessage = function (state, msg) {
   var _state = clone(state)
-  _state.received = msg.sequence
+  _state.received = Math.max(state.received || 0, msg.sequence)
 
-  if(!state.receiving) {
-    //we should not have received a message if we are in sending state!
-    _state.error = true
-  }
-  else if(state.receiving) {
-    if(state.local[msg.author] > msg.sequence) {//we already know this msg
-      _state.ready = {id: msg.author, seq: -1*state.local[msg.author]}
+  var seq = state.local[msg.author]
+  if(msg.sequence <= seq) {
+    //we already know this, please shut up!
+    if(state.receiving) {
       _state.receiving = false
+      _state.ready = {id: msg.author, seq: - seq}
     }
-    else if(state.local[msg.author] + 1 == msg.sequence)
-      _state.effect = {action: 'append', value: msg}
-      ; //SIDE EFFECT: ready to validate
-    else
-      ; //ignore
   }
+  else if(msg.sequence == seq + 1) {
+    //they said what we where going to
+    if(state.ready) _state.ready = null
+    _state.effect = {action: 'append', value: msg}
+  }
+  else
+    _state.error = true
 
   return _state
 }
@@ -74,54 +74,6 @@ exports.receiveNote = function (state, note) {
   }
 
   return _state
-
-//  _state.received = Math.max(Math.abs(note.seq), _state.has || 0)
-//  if(!state.sending)
-//    if(note.seq > 0) {
-//      _state.sending = true
-//    }
-//  if(state.sending) {
-//    if(note.seq < 0) {
-//      _state.sending = false
-//      _state.ready = null
-//    }
-//
-//    //they know about a message we don't yet, go into receiving mode
-//    if(Math.abs(note.seq) > state.local[note.id] && !state.receiving) {
-//      _state.receiving = true
-//      _state.sending = false
-//      //ready for next request.
-//      _state.ready = {id: note.id, seq: state.local[note.id]}
-//    }
-//
-//    //we where about to send a message, but they asked for an older one (weird)
-//    if(state.ready && state.ready.sequence <= note.seq) {
-//      _state.ready = null
-//      _state.effect = {action: 'get', value: note}
-//      //SIDE EFFECT: retrive next message
-//    }
-//  }
-//  else if(state.recieving) {
-//    //generally shouldn't happen but
-//    //could if we have just switched to receiving but they didn't get the message yet
-//  }
-//  else if(!state.receiving) {
-//    if(state.local[note.id] < Math.abs(note.seq)) {
-//      _state.receiving = true
-//      _state.ready = {id: note.id, seq: state.local[note.id]} //request this feed from our current value.
-//    }
-//    else if(note.seq > 0) {
-//      _state.sending = true
-//      if(state.local[note.id] > note.seq) {
-//        if(!isMessage(state.ready) || state.ready.sequence !== note.sequence) {
-//          _state.ready = null
-//          _state.effect = {action: 'get', value: {id: note.id, seq: note.seq+1}}
-//        }
-//      }
-//    }
-//  }
-//
-  return _state
 }
 
 //we have either written a new message ourselves,
@@ -146,6 +98,11 @@ exports.retriveMessage = function (state, msg) {
   //otherwise, the next retrival will be triggered by READ
   return _state
 }
+
+
+
+
+
 
 
 
