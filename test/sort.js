7637d076ba230ded531a619138416676c291d614
Dominic Tarr <dominic.tarr@gmail.com>
2017-03-25 19:57:18 +1300

    fix test dep

--- test/sort.js
+++ test/sort.js
@@ -1,5 +1,5 @@
 var tape = require('tape')
-var EBT = require('../source')
+var u = require('../util')
 
 tape('set sorting', function (t) {
 
@@ -14,7 +14,7 @@ tape('set sorting', function (t) {
   while(ary.length) {
     for(var i = 0; i < 10; i++) {
       var _ary = ary.slice().sort(function () { return Math.random() - 0.5 })
-      t.deepEqual(EBT.sort(_ary), ary)
+      t.deepEqual(u.sort(_ary), ary)
     }
     ary.shift()
   }
