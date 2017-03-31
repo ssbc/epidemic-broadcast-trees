0f29242d0f0e9d584353897caa40b60b839aacd7
Dominic Tarr <dominic.tarr@gmail.com>
2017-03-31 21:45:42 +1300

    test file for when i discover new edge cases

---
+++ test/edges.js
@@ -0,0 +1,61 @@
+var states = require('../state')
+var tape = require('tape')
+var u = require('../util')
+
+function clone (obj) {
+  return JSON.parse(JSON.stringify(obj))
+}
+
+tape('receive remote note before we sent ours', function (t) {
+
+  var msg = {sequence: 3, author: 'alice', content: 'blah'}
+
+  var state = states.init(2)
+  t.equal(state.ready, 2) //assume that a request for message 2 has been sent.
+
+//  state = states.read(state)
+  
+  //we received their note, but have not sent ours yet.
+  state = states.receiveNote(state, 1)
+
+  t.equal(state.local.req, null)
+  t.equal(state.remote.req, 1)
+
+  //since when we receive their note we know we are behind
+  //assume they are in tx mode, but we are not.
+//  t.equal(state.local.tx, true, 'since we are behind, we should not transmit')
+//  t.equal(state.remote.tx, true, 'since remote is ahead, they should transmit')
+
+  t.deepEqual(state, {
+    local: {seq: 2, req: null, tx: true},
+    remote: {seq: null, req: 1, tx: false},
+    ready: 2,
+    effect: null
+  })
+
+
+//  var _state = states.receiveMessage(clone(state), msg)
+//
+//  t.deepEqual(_state, {
+//    local: {seq: 2, req: 2, tx: false},
+//    remote: {seq: 3, req: 10, tx: true},
+//    ready: null,
+//    effect: msg
+//  })
+//
+//  _state.effect = null //assume this was appended
+//
+//  _state = states.appendMessage(_state, msg)
+//
+//  t.deepEqual(_state, {
+//    local: {seq: 3, req: 2, tx: false},
+//    remote: {seq: 3, req: 10, tx: true},
+//    ready: null,
+//    effect: null
+//  })
+//
+
+  t.end()
+})
+
+
