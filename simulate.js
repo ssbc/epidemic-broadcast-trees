06da05f7e5d27a6e5934b07d59eaef6a3d6c8045
Dominic Tarr <dominic.tarr@gmail.com>
2017-04-01 15:58:38 +1300

    move toward network simulator

---
+++ simulate.js
@@ -0,0 +1,165 @@
+var states = require('./state')
+var RNG = require('rng')
+var deepEqual = require('deep-equal')
+//state is {source, sink, nodeState, log, old_length}
+
+var model = exports.model = function (pState, cState, random) {
+  /*
+    we want to handle appends (and thus validation) before handling any receives.
+    we can handle get and send in parallel though.
+
+  */
+
+  var acts = {}
+  //emit is shared
+  if(pState.emit || cState.source.length || isMessage(cState.nodeState.effect)) //connection
+    acts.ordered = true
+  if(cState.nodeState.ready != null)
+    acts.send = true
+  if(isNote(cState.nodeState.effect))
+    acts.get = true
+
+  var keys = Object.keys(acts)
+
+  var key = keys[~~(random*keys.length)]
+
+  if(key === 'send') {
+    var data = cState.nodeState.ready
+    cState.sink.push(data)
+    cState.nodeState = states.read(cState.nodeState)
+  }
+  else if(key === 'get') {
+    var msg = pState.log[cState.nodeState.effect - 1] //shared
+    cState.nodeState.effect = null
+    cState.nodeState = states.gotMessage(cState.nodeState, msg)
+  }
+  else if(key == 'ordered'){
+    //this bit should fire an event on all the connection states
+    if(pState.emit) {
+      var msg = pState.emit
+      pState.emit = null
+      cState.nodeState = states.appendMessage(cState.nodeState, msg)
+    }
+    else if(isMessage(cState.nodeState.effect)) {
+      pState.emit = cState.nodeState.effect
+      pState.log.push(pState.emit) //shared
+      cState.nodeState.effect = null
+    }
+    else if(cState.source.length) {
+      var data = cState.source.shift() //connection.
+      if(data == null) throw new Error('should never read null/undefined from network')
+      cState.nodeState = (isMessage(data) ? states.receiveMessage : states.receiveNote)(cState.nodeState, data)
+    }
+    else throw new Error('should not have ran out of options')
+  }
+
+  return [pState, cState]
+}
+
+function isMessage(data) { return data && 'object' === typeof data }
+function isNote(n) { return Number.isInteger(n) }
+
+exports.peer = function peer (network, id, log) {
+  if(!log) log = []
+
+  network[id] = {
+    id: id, log: log, emit: false,
+    connections: {}
+  }
+
+  return network
+}
+
+exports.connection = function connection (network, from, to) {
+  network[from].connections[to] = {
+    source: [], sink: [], nodeState: states.init(network[from].log.length),
+    id: from, remote: to, log: network[from].log
+  }
+  network[to].connections[from] = {
+    source: [], sink: [], nodeState: states.init(network[to].log.length),
+    id: to, remote: from, log: network[to].log
+  }
+  return network
+}
+
+exports.countConnections = function countConnections (network) {
+  //count unidirectional connections then divide by 2 to get duplex connections
+  var uniplex = 0
+  for(var k in network) {
+    uniplex += Object.keys(network[k].connections).length
+  }
+  return uniplex / 2
+}
+
+exports.isConsistent = function isConsistent (network) {
+  //this is the "handshaking lemma" if I recall
+  var keys = Object.keys(network)
+  for(var i = 0; i < keys.length; i++) {
+    var k = keys[i]
+    for(var j = i+1; j < keys.length; j++) {
+      var l = keys[j]
+      if(!deepEqual(network[k].log, network[l].log, 'peer:'+k +' is consistent with :'+j))
+        return false
+    }
+  }
+  return true
+}
+
+var hasWork = exports.hasWork = function hasWork (pState, cState) {
+  return (
+    pState.emit || cState.source.length ||
+    cState.nodeState.ready != null ||
+    cState.nodeState.effect != null
+  )
+}
+
+exports.evolveNetwork = function evolveNetwork (network, msglog, seed) {
+  var rng = new RNG.MT(seed)
+
+  function random () {
+    return rng.random()
+  }
+
+  function isWaiting() {
+    for(var k in network)
+      for(var j in network[k].connections)
+        if(hasWork(network[k], network[k].connections[j])) {
+          return true
+        }
+  }
+
+  function randomValue(obj) {
+    var k = Object.keys(obj)
+    return obj[k[~~(random()*k.length)]]
+  }
+
+  while(isWaiting()) {
+    var pState = randomValue(network)
+    var cState = randomValue(pState.connections)
+    if(cState) {
+      var r = model(pState, cState, random())
+      pState = r[0]
+      cState = r[1]
+
+      if(cState.nodeState.local.tx == false)
+        throw new Error('transmit should always be true in this test')
+
+      if(cState.nodeState.error)
+        throw new Error('error state')
+
+      //copy from the sink to the source immediately, since it gets read randomly anyway.
+      if(cState.sink.length)
+        console.log('send', cState.id+'->'+cState.remote, cState.sink)
+
+      while(cState.sink.length) {
+        if(cState.sink[0] == null) throw new Error('cannot send null')
+        var data = cState.sink.shift()
+        msglog.push({from: cState.id, to: cState.remote, data: data})
+        network[cState.remote].connections[cState.id].source.push(data)
+      }
+    }
+  }
+  return network
+}
+
+
--- test/simulate.js
+++ test/simulate.js
@@ -1,49 +1,68 @@
-
+var u = require('../util')
+//var model = require('../model')
+//var states = require('../state')
 var tape = require('tape')
+var deepEqual = require('deep-equal')
+var RNG = require('rng')
 
-var model = require('./model')
+var sim = require('../simulate')
 
-tape('a3-> b0', function (t) {
+//state is {source, sink, nodeState, log, old_length}
 
-  var ab = [], ba = []
+function run (t, seed) {
 
-  var a_log = [
-    {author: 'alice', sequence: 1, content: 'hi'},
-    {author: 'alice', sequence: 2, content: 'hello'},
-    {author: 'alice', sequence: 3, content: 'houdy'}
-  ]
+  var rng = new RNG.MT(seed)
 
-  var b_log = []
-  var a = model(ab, ba, a_log)
-  var b = model(ba, ab, b_log)
-
-  var states = { alice: a.state, bob: b.state }
-
-  var transitions = [a, b]
-
-  function process () {
-    transitions.sort(function () { return Math.random() - 0.5 })
-    for(var i = 0; i < transitions.length; i++)
-      if(transitions[i]() !== false) return true //found a match
-    return false
+  function random () {
+    return rng.random()
   }
 
-  var i = 0
-  console.log(i, states)
-  while(process(i++))
-    console.log(i, states)
+  var a_log = [
+    {author: 'a', sequence: 1, content: 'a'},
+    {author: 'a', sequence: 2, content: 'b'},
+    {author: 'a', sequence: 3, content: 'c'}
+  ]
 
-  t.end()
-})
+  var network = {}
+  network = sim.peer(network, 'A', a_log)
+  network = sim.peer(network, 'B', [])
+  network = sim.connection(network, 'A', 'B')
+
+  //initialize
+
+  var msglog = []
+
+  network = sim.evolveNetwork(network, msglog, seed)
+
+  t.equal(msglog.filter(function (e) {
+    return u.isNote(e.data)
+  }).length, sim.countConnections(network)*2, 'exactly two notes are sent')
+
+  t.ok(sim.isConsistent(network), 'Network is consistent')
 
 
+  //add one more item to A's log
+  a_log.push({author: 'a', sequence: 4, content: 'LIVE'})
+  network.A.emit = {author: 'a', sequence: 4, content: 'LIVE'}
 
+  t.ok(sim.hasWork(network.A, network.A.connections.B))
+  network = sim.evolveNetwork(network, msglog, random())
+  t.ok(sim.isConsistent(network))
 
+}
 
-
-
-
-
+if(process.argv[2])
+  tape('run 3 message test with 2 peers, seed:'+ (+process.argv[2]), function (t) {
+    run(t, +process.argv[2])
+    t.end()
+  })
+else
+  for(var i = 0; i < 100; i++) (function (i) {
+    tape('run 3 message test with 2 peers, seed:'+i, function (t) {
+      run(t, i)
+      t.end()
+    })
+  })(i)
 
 
 
