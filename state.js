057e82b4dbc78d948b4def2a235c0fc84d5b9f93
Dominic Tarr <dominic.tarr@gmail.com>
2017-04-02 21:06:11 +1200

    handle when messages pass in flight

--- state.js
+++ state.js
@@ -1,3 +1,4 @@
+'use strict'
 var u = require('./util')
 var isMessage = u.isMessage
 var isNote = u.isNote
@@ -43,6 +44,9 @@ exports.read = function (state) {
   var _ready = state.ready
   state.ready =  null
   if(isMessage(_ready)) {
+    if(state.remote.seq != null && Math.max(state.remote.seq, state.remote.req) +1 !== _ready.sequence) {
+      throw new Error('out of order!')
+    }
     state.remote.seq = _ready.sequence
     state.local.req = Math.max(state.local.req, _ready.sequence)
   } else {
@@ -67,9 +71,8 @@ function isNextRxMessage(state, msg) {
 function isNextTxMessage (state, msg) {
   return (
     !isInitRx(state) &&
-//    state.local.tx &&
     state.remote.req < msg.sequence &&
-    msg.sequence === (Math.max(state.remote.seq, state.remote.req) + 1)
+    msg.sequence === Math.max(state.remote.seq, state.remote.req) + 1
   )
 }
 
@@ -77,13 +80,32 @@ function isNextTxMessage (state, msg) {
 exports.receiveMessage = function (state, msg) {
   if(!isMessage(msg)) throw new Error('expected a Message!')
   var _state = clone(state)
+
+//  if(state.remote.tx === false) {
+//    console.log('received msg, when !tx')
+//    console.log(msg, state)
+//  }
+//
   _state.remote.req = Math.max(state.remote.req || 0, msg.sequence)
-  _state.remote.seq = msg.sequence
+  _state.remote.seq = Math.max(state.remote.seq || 0, msg.sequence)
+
+  if(state.remote.seq > msg.sequence) {
+//    console.log('receiveMessage', state, msg)
+//    throw new Error('remote sent out of order')
+  }
+  //we may have sent something to them
+ // _state.remote.seq = Math.max(msg.sequence, _state.remote.seq)
 
   if(state.remote.tx == null)
     throw new Error('we received a message, when we where waiting for remote to send initial request')
 
   var seq = state.local.seq
+  if(isMessage(state.ready)) {
+    if(state.ready.sequence <= msg.sequence) {
+      state.ready = null
+      //throw new Error('about to send message we just received')
+    }
+  }
   if(isOldMessage(state, msg)) {
     //we already know this, please shut up!
     if(state.remote.tx) {
@@ -98,13 +120,14 @@ exports.receiveMessage = function (state, msg) {
     if(state.ready != null)
       _state.ready = null
     _state.effect = msg
-
+    if(state.remote.tx == false)
+      state.ready = state.local.seq
     //update local seq, instead of calling to append?
     //so that we can handle the next incomming?
     //what if an invalid message arrives? we should abort this receiver...
   }
   else {
-    console.log('ERROR MESSAGE', msg, state)
+    //this means something went really wrong
     _state.error = true
   }
   return _state
@@ -115,14 +138,18 @@ exports.receiveNote = function (state, note) {
   var _state = clone(state)
   var seq = state.local.seq
   var requested = note >= 0
-  var _seq = Math.abs(note)
+  var _seq = Math.max(Math.abs(note), state.remote.seq)
 
+//  if(state.local.tx != requested) {
+//    console.log('enable tx')
+//  }
+//
   _state.local.tx = requested
-  _state.remote.req = _seq
+  _state.remote.req = Math.max(_seq, _state.remote.req)
 
   if(state.local.req == null) return _state
 
-  if(isMessage(state.ready) && _seq > state.ready.sequence)
+  if(isMessage(state.ready) && _seq >= state.ready.sequence)
       state.ready = null
 
   //if they sent a note which is ahead of us, and we did not previously send something.
@@ -146,22 +173,39 @@ exports.appendMessage = function (state, msg) {
   _state.local.seq = msg.sequence
 
   if(state.local.tx) {
-    if(isNextTxMessage(state, msg))
-      _state.ready = state.local.tx ? msg : msg.sequence
+    if(isNextTxMessage(state, msg)) {
+      _state.ready = msg
+    }
     else if(isNote(state.ready)) { //this should only happen when it is the initial request
+      //if we are transmitting, why would we be sending a note?
       //if it's back to even, we don't need to send a message, but if we are not
       //then the message has meaning.
-      _state.ready = msg.sequence === Math.max(state.remote.seq, state.remote.req) ? null : msg.sequence
+      //if(state.remote.tx)
+//      _state.ready = msg.sequence === Math.max(state.remote.seq, state.remote.req) ? null : msg.sequence
+      if(state.local.req != null) {
+        _state.ready = null
+        if(_state.local.seq > Math.max(state.remote.seq, state.remote.req))
+          _state.effect = Math.max(state.remote.seq, state.remote.req) + 1
+      }
     }
     else if(!isMessage(_state.ready))
       _state.ready = null
+
+      if(state.local.seq > Math.max(state.remote.req,state.remote.seq))
+        state.effect = Math.max(state.remote.req,state.remote.seq) + 1
+//    if(msg.sequence > state.remote.seq) {
+//      //console.log('can send!!', msg, state)
+//      state.ready = state.remote.seq + 1
+//    }
   }
   else if(!state.local.tx) {
     //unless we know they are up to this, send a note
-    if(msg.sequence >= state.remote.req)
+    if(msg.sequence > state.remote.req) {
+      //console.log('APPEND_TURN_OFF_R_TX', msg, state)
       _state.ready = state.remote.tx ? msg.sequence : -msg.sequence //SEND NOTE
+    }
     else if(isNote(state.ready) && state.ready > 0)
-      state.ready = message.sequence //UPDATE NOTE
+      state.ready = msg.sequence //UPDATE NOTE
     else
       state.ready = null
   }
@@ -187,5 +231,7 @@ exports.gotMessage = function (state, msg) {
   ;
   return _state
 }
+
+
 
 
