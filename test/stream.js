7f538c76138ecae8db3bc6399c39dd69f2c4b1da
Dominic Tarr <dominic.tarr@gmail.com>
2017-04-03 23:08:12 +1200

    repeat simple stream tests, to ensure that they actually work and become spanning trees!

--- test/stream.js
+++ test/stream.js
@@ -1,22 +1,37 @@
-
+var tape = require('tape')
 var Stream = require('../stream')
 var S = require('../state')
 var pull = require('pull-stream')
 
+
+
 function Peer (logs) {
+  logs._append = logs._append || []
+
+  function onAppend (msg) {
+    for(var i = 0; i < logs._append.length; i++)
+      logs._append[i](msg)
+  }
 
   return function () {
     var states = {}
     for(var k in logs)
-      states[k] = S.init(logs[k].length)
+      if(k[0] != '_')
+        states[k] = S.init(logs[k].length)
 
     var stream = Stream(states, function get (id, seq, cb) {
       cb(null, logs[id][seq - 1])
     }, function append (msg, cb) {
-      logs[msg.author].push(msg)
-      stream.onAppend(msg)
-      cb()
+      if(logs[msg.author] && msg.sequence == logs[msg.author].length + 1) {
+        logs[msg.author].push(msg)
+        onAppend(msg)
+        cb()
+      }
+      else cb(new Error('could not append'))
     })
+
+    logs._append.push(stream.onAppend)
+
     return stream
   }
 }
@@ -32,35 +47,136 @@ var bob = [
   {author: 'bob', sequence: 3, content: 'Z'}
 ]
 
-var alice_db, bob_db
-var alice_stream = Peer(alice_db = {alice: alice, bob: []}) ()
-var bob_stream = Peer(bob_db = {alice: [], bob: bob}) ()
+tape('two peers', function (t) {
 
-function log (name) {
-  return pull.through(function (data) {
-    console.log(name, data)
-  })
-}
+  var alice_db, bob_db
+  var alice_stream = Peer(alice_db = {alice: alice, bob: []}) ()
+  var bob_stream = Peer(bob_db = {alice: [], bob: bob}) ()
 
-pull(
-  alice_stream,
-  log('a->b'),
-  bob_stream,
-  log('b->a'),
-  alice_stream
-)
+  function log (name) {
+    return pull.through(function (data) {
+      console.log(name, data)
+    })
+  }
+
+  pull(
+    alice_stream,
+    log('a->b'),
+    bob_stream,
+    log('b->a'),
+    alice_stream
+  )
+
+  delete alice_db._append
+  delete bob_db._append
+  console.log(alice_db)
+  console.log(bob_db)
+  t.deepEqual(alice_db, bob_db, 'databases are consistent')
+  t.end()
+})
+
+tape('three peers, two streams', function (t) {
+
+  var alice_db = {alice: alice, bob: []}
+  var bob_db = {alice: [], bob: bob}
+  var charles_db = {alice: alice.slice(0, 2), bob: bob.slice(0, 1)}
+
+  var alice_stream = Peer(alice_db) ()
+  var bob_stream = Peer(bob_db) ()
+  var bob_stream2 = Peer(bob_db) ()
+  var charles_stream = Peer(charles_db) ()
+
+  function log (name) {
+    return pull.through(function (data) {
+      console.log(name, data)
+    })
+  }
+
+  pull(
+    alice_stream,
+    log('a->b'),
+    bob_stream,
+    log('b->a'),
+    alice_stream
+  )
+
+  pull(
+    bob_stream2,
+    log('b->c'),
+    charles_stream,
+    log('b->c'),
+    bob_stream2
+  )
+
+  delete alice_db._append
+  delete bob_db._append
+  delete charles_db._append
+
+  console.log(alice_db)
+  console.log(bob_db)
+  console.log(charles_db)
+
+  t.deepEqual(alice_db, bob_db, 'databases are consistent')
+  t.deepEqual(alice_db, charles_db, 'databases are consistent')
+  t.end()
+})
+
+tape('three peers, four streams', function (t) {
+
+  var alice_db = {alice: alice, bob: []}
+  var bob_db = {alice: [], bob: bob}
+  var charles_db = {alice: alice.slice(0, 2), bob: bob.slice(0, 1)}
+
+  var alice_stream = Peer(alice_db) ()
+  var alice_stream2 = Peer(alice_db) ()
+  var bob_stream = Peer(bob_db) ()
+  var bob_stream2 = Peer(bob_db) ()
+  var charles_stream = Peer(charles_db) ()
+  var charles_stream2 = Peer(charles_db) ()
+
+  function log (name) {
+    return pull.through(function (data) {
+      console.log(name, data)
+    })
+  }
+
+  pull(
+    alice_stream,
+    log('a->b'),
+    bob_stream,
+    log('b->a'),
+    alice_stream
+  )
+
+  pull(
+    bob_stream2,
+    log('b->c'),
+    charles_stream,
+    log('c->b'),
+    bob_stream2
+  )
+
+  pull(
+    alice_stream2,
+    log('a->c'),
+    charles_stream2,
+    log('c->a'),
+    alice_stream2
+  )
 
 
+  delete alice_db._append
+  delete bob_db._append
+  delete charles_db._append
 
-console.log(alice_db)
-console.log(bob_db)
+  console.log(alice_db)
+  console.log(bob_db)
+  console.log(charles_db)
 
-
-
-
-
-
-
+  t.deepEqual(alice_db, bob_db, 'databases are consistent')
+  t.deepEqual(alice_db, charles_db, 'databases are consistent')
+  t.end()
+})
 
 
 
