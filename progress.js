f2f78b3c6e5d61ad7ed486b87e24d0b112faaebb
Dominic Tarr <dominic.tarr@gmail.com>
2017-04-24 16:35:05 +1200

    pass progress tests

--- progress.js
+++ progress.js
@@ -6,6 +6,40 @@ function max (a, b) {
   return a == null ? b : b == null ? a : Math.max(a, b)
 }
 
+function process(data, state) {
+  var local = state.local
+  var remote = state.remote
+  var _seq = max(remote.seq, remote.req)
+  var seq = max(local.seq, local.req)
+
+  //don't count this feed, because we do not expect
+  //to exchange anything.
+  //this should only happen when there are many connections.
+  if(!local.tx && seq > _seq) return data
+
+  data.feeds ++
+
+
+  //req represents what they _know_ we have because
+  //we have either mentioned it in a note or sent it.
+  data.total = Math.max(
+    seq - remote.req,
+    _seq - (local.req || local.seq)
+  )
+
+  if(local.seq == _seq)
+    data.sync ++
+  else if(local.seq == null || _seq == null)
+    data.unknown ++
+  else if(local.seq > _seq && local.tx) {
+    data.send += local.seq - _seq
+  } else if(local.seq < _seq && remote.tx) {
+    data.recv += _seq - local.seq
+  }
+
+  return data
+}
+
 module.exports = function (states) {
   var data = {
     sync: 0, feeds: 0,
@@ -13,36 +47,13 @@ module.exports = function (states) {
     unknown: 0,
   }
 
-  var sync = 0, recv = 0, send = 0, unknown = 0, feeds = 0
-  for(var k in states) {
-    data.feeds ++
-    var local = states[k].local
-    var remote = states[k].remote
-
-    var _seq = max(remote.seq, remote.req)
-    var seq = max(local.seq, local.req)
-
-    //req represents what they _know_ we have because
-    //we have either mentioned it in a note or sent it.
-    data.total = Math.max(
-      seq - remote.req,
-      _seq - (local.req || local.seq)
-    )
-
-    if(local.seq == _seq)
-      data.sync ++
-    else if(local.seq == null || _seq == null)
-      data.unknown ++
-    else if(local.seq > _seq && local.tx) {
-      data.send += local.seq - _seq
-    } else if(local.seq < _seq && remote.tx) {
-      data.recv += _seq - local.seq
-    }
-  }
+  for(var k in states)
+    //Only count this as an out of sync feed if we either can send
+    //to it or receive from it.
+    data = process(data, states[k])
 
   return data
 }
-
 
 
 
