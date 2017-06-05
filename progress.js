94323e83db9cb396306652b19eae7ba4933c2dce
Dominic Tarr <dominic.tarr@gmail.com>
2017-06-05 20:34:37 +1200

    progress should reflect how many messages we expect to send

--- progress.js
+++ progress.js
@@ -9,32 +9,44 @@ function max (a, b) {
 function process(data, state) {
   var local = state.local
   var remote = state.remote
-  var _seq = max(remote.seq, remote.req)
-  var seq = max(local.seq, local.req)
+  var _seq = remote.req == -1 ? -1 : max(remote.seq, remote.req)
+  var seq = local.req == -1 ? -1 : max(local.seq, local.req)
 
   //don't count this feed, because we do not expect
   //to exchange anything.
   //this should only happen when there are many connections.
   if(!local.tx && seq > _seq) return data
 
-  data.feeds ++
-
-
   //req represents what they _know_ we have because
   //we have either mentioned it in a note or sent it.
-  data.total = Math.max(
-    seq - remote.req,
-    _seq - (local.req || local.seq)
-  )
 
-  if(local.seq == _seq)
-    data.sync ++
-  else if(local.seq == null || _seq == null)
+  if(seq == null) {
+    //we havn't decided if we want this feed yet
     data.unknown ++
-  else if(local.seq > _seq && local.tx) {
-    data.send += local.seq - _seq
-  } else if(local.seq < _seq && remote.tx) {
-    data.recv += _seq - local.seq
+  } else if(seq === -1) {
+    //we have decided we do not want this feed. don't count it.
+  } else {
+    if(_seq == null) {
+      data.unknown ++
+    } else if(_seq === -1) {
+      //they have told us they do not want it.
+      //this means we do not expect to send anything.
+      //so don't count this feed.
+    } else {
+      data.feeds ++
+
+      if(seq == _seq)
+        data.sync ++
+
+      data.total += Math.max(
+        seq - remote.req,
+        _seq - (local.req || local.seq)
+      )
+      if(seq > _seq && local.tx)
+        data.send += seq - _seq
+      else if(seq < _seq && remote.tx)
+        data.recv += _seq - seq
+    }
   }
 
   return data
@@ -54,9 +66,6 @@ module.exports = function (states) {
 
   return data
 }
-
-
-
 
 
 
