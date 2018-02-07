d191db5931f6b1e00278f86ff8b0aab6b820a3bc
Dominic Tarr <dominic.tarr@gmail.com>
2018-02-07 16:19:00 +1300

    push-stream & baseclass

---
+++ index.js
@@ -0,0 +1,63 @@
+var Stream = require('./stream')
+var events = require('./events')
+
+module.exports = function (opts) {
+  var state = events.initialize(opts.id)
+  state.clock = {}
+  var self = {
+    id: opts.id,
+    streams: {},
+    state: state,
+    request: function (id, follows) {
+      self.state = events.follow(self.state, {id: id, value: follows})
+      self.update()
+    },
+    createStream: function (remote_id) {
+      var stream = this.streams[remote_id] = new Stream(this, remote_id)
+      opts.getClock(remote_id, function (err, clock) {
+        stream.clock(err ? {} : clock)
+      })
+      return stream
+    },
+    _retrive: function (err, msg) {
+      if(msg) {
+        self.state = events.retrive(self.state, msg)
+        this.update()
+      }
+      else {
+        //this should never happen.
+        //replication for this feed is in bad state now.
+        console.log('could not retrive msg:', err)
+      }
+    },
+    _append: function (err, msg) {
+      if(msg) {
+        self.state = events.append(self.state, msg)
+        self.update()
+      }
+      else
+        //this definitely can happen.
+        //TODO: broadcast fork proofs
+        console.log('error appending:', err)
+    },
+    update: function () {
+      //retrive next messages.
+      for(var id in this.state.peers) {
+        var state = this.state.peers[id]
+        while(state.retrive.length) {
+          var id = state.retrive.shift()
+          if(state.replicating[id])
+            opts.getAt({id: id, seq:state.replicating[id].sent+1}, this._retrive)
+        }
+      }
+      if(this.state.receive.length) {
+        var msg = this.state.receive.shift()
+        opts.append(msg, this._append)
+      }
+      for(var k in this.streams)
+        this.streams[k].resume()
+    },
+  }
+  return self
+}
+
---
+++ stream.js
@@ -0,0 +1,71 @@
+var inherits = require('inherits')
+var events = require('./events')
+
+function isMsg (m) {
+  return Number.isInteger(m.sequence) && m.sequence > 0 && 'string' == typeof m.author && m.content
+}
+
+module.exports = EBTStream
+
+function EBTStream (peer, remote) {
+  this.paused = true //start out paused
+  this.remote = remote
+  this.peer = peer
+  this.peer.state =
+    events.connect(this.peer.state, {id: remote})
+
+  this.sink = this.source = null
+}
+
+EBTStream.prototype.clock = function (clock) {
+  this.peer.state =
+    events.peerClock(this.peer.state, {id: this.remote, value: clock})
+  this.paused = false
+  if(this.source) this.source.resume()
+}
+
+EBTStream.prototype.write = function (data) {
+  if(isMsg(data))
+    this.peer.state =
+      events.receive(this.peer.state, {id: this.remote, value:data})
+  else
+    this.peer.state =
+      events.notes(this.peer.state, {id: this.remote, value: data})
+
+  this.peer.update(this.remote)
+}
+
+EBTStream.prototype.end = function (err) {
+  this.peer.state =
+    events.disconnect(this.peer.state, {id: remote})
+
+  //remove from the peer...
+  delete this.peer.streams[this.remote]
+}
+
+EBTStream.prototype.canSend = function () {
+  var state = this.peer.state.peers[this.remote]
+  return (
+    this.sink &&
+    !this.sink.paused && (
+      state.msgs.length || state.notes
+    )
+  )
+}
+
+EBTStream.prototype.resume = function () {
+  var state = this.peer.state.peers[this.remote]
+  if(!this.sink || this.sink.paused) return
+  while(this.canSend()) {
+    if(state.msgs.length)
+      this.sink.write(state.msgs.shift())
+    else {
+      var notes = state.notes
+      state.notes = null
+      this.sink.write(notes)
+    }
+  }
+}
+
+EBTStream.prototype.pipe = require('push-stream/pipe')
+
