459a4f6b0e7fb4b8a6954a49f8d11c9aec3b38d9
Dominic Tarr <dominic.tarr@gmail.com>
2018-02-16 20:33:26 +1300

    test connecting a second stream to the same peer ends the first one

--- test/stream.js
+++ test/stream.js
@@ -22,6 +22,7 @@ function create (id) {
     getClock: function (id, cb) {
       cb(null, {})
     },
+    setClock: function () {},
     getAt: function (pair, cb) {
       if(!store[pair.id] || !store[pair.id][pair.sequence]) cb(new Error('not found'))
       else cb(null, store[pair.id][pair.sequence])
@@ -65,4 +66,51 @@ test('a<->b', function (t) {
   t.end()
 
 })
+
+test('a<->b,b', function (t) {
+
+  var alice = create('alice')
+  var bob = create('bob')
+
+  alice.request('alice', true)
+  alice.request('bob', true)
+  bob.request('alice', true)
+  bob.request('bob', true)
+
+  var as = alice.createStream('bob')
+  var bs = bob.createStream('alice')
+
+  console.log('initial.alice:',alice.progress())
+  console.log('initial.bob  :',bob.progress())
+
+  as.pipe(bs).pipe(as)
+
+  alice.append({author: 'alice', sequence: 1, content: 'hello'}, function () {})
+  bob.append({author: 'bob', sequence: 1, content: 'hello'}, function () {})
+
+  console.log(bob.store)
+  console.log(alice.store)
+
+  console.log('final.alice:',alice.progress())
+  console.log('final.bob  :',bob.progress())
+
+  t.deepEqual(alice.store, bob.store)
+
+  var bs2 = bob.createStream('alice')
+  var as2 = alice.createStream('bob')
+
+  as2.pipe(bs2).pipe(as2)
+
+  bob.append({author: 'bob', sequence: 2, content: 'hello2'}, function () {})
+
+  t.equal(as.ended, true)
+  t.equal(bs.ended, true)
+
+  t.deepEqual(alice.store, bob.store)
+
+  t.end()
+
+})
+
+
 
