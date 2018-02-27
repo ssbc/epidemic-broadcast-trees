69904785b0608180cab179347ff274f0f87f3099
Dominic Tarr <dominic.tarr@gmail.com>
2018-02-28 12:58:22 +1300

    test that clients wait for server to send first note before sending theirs

---
+++ test/client.js
@@ -0,0 +1,36 @@
+
+var test = require('tape')
+
+var events = require('../events')(require('./options'))
+
+var note = events.note
+
+test('client does not send {} until received it', function (t) {
+  var state = events.initialize('alice')
+  state = events.connect(state, {id: 'bob', client: true})
+  state.follows = {alice: true, bob: true}
+  state.clock = {alice:1, bob: 1}
+  state = events.peerClock(state, {id: 'bob', value: {}})
+  t.equal(state.peers.bob.notes, null)
+  state = events.notes(state, {id: 'bob', value: {}})
+  t.deepEqual(state.peers.bob.notes, {alice: note(1, true), bob: note(1, true)})
+
+  state.peers.bob.notes = null
+
+
+  t.end()
+})
+
+test('client does not send {} until received it, but will send empty note, one time', function (t) {
+  var state = events.initialize('alice')
+  state = events.connect(state, {id: 'bob', client: true})
+  state = events.peerClock(state, {id: 'bob', value: {}})
+  t.equal(state.peers.bob.notes, null)
+  state = events.notes(state, {id: 'bob', value: {}})
+  t.deepEqual(state.peers.bob.notes, {})
+  t.end()
+})
+
+
+
+
