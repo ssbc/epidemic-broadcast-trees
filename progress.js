d67be78b5fbce083aafce0003fed64e46cf10036
Dominic Tarr <dominic.tarr@gmail.com>
2018-03-01 14:16:25 +1300

    better progress algorithm

--- progress.js
+++ progress.js
@@ -1,37 +1,43 @@
 
+/*
+  better progress algorithm:
+
+  count number of feeds we expect to send to each peer.
+  sum of the difference between what they have asked, and what we have sent.
+
+  same for receive, but have to remember what we asked for.
+
+*/
+
 module.exports = function (state) {
   var prog = {start:0, current: 0, target: 0}
-  for(var peer in state.peers)
-    for(var feed in state.peers[peer].replicating) {
-      var rep = state.peers[peer].replicating[feed]
+  for(var peer_id in state.peers)
+    var peer = state.peers[peer_id]
+
+//    prog.target ++
+//    if(peer.replicating) prog.current ++
+
+    for(var feed_id in peer.replicating) {
+      var rep = peer.replicating[feed_id]
       //progress for sending initial note
       prog.target ++
       if(rep.sent != null) prog.current ++
+
       prog.target ++
-      var peerClock = state.peers[peer].clock
-      if(peerClock) {
-        prog.current ++
+      if(rep.requested != null) prog.current ++
 
-        var seq1 = Math.max(peerClock[feed] || 0, rep.sent), seq2 = state.clock[feed] || 0
-        prog.target += Math.max(seq1, seq2) - Math.min(seq1, seq2)
-
+      var seq = peer.clock[feed_id]
+      var lseq = state.clock[feed_id] || 0
+      if(rep.rx && rep.requested != null && rep.requested > -1 && lseq < seq) {
+        prog.current += lseq - rep.requested
+        prog.target += seq - rep.requested
+          //Math.max(seq - rep.requested, lseq - rep.requested)
+      }
+      if(rep.tx && seq > -1 && seq < lseq) {
+        prog.current += rep.sent - seq
+        prog.target += lseq - seq
       }
     }
   return prog
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
