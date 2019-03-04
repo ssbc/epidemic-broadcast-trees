a5efedda677b81f581d0601b666309609d70da9b
Dominic Tarr <dominic.tarr@gmail.com>
2019-03-04 16:25:53 +1300

    clear up edge cases around forks

--- v3.js
+++ v3.js
@@ -1,6 +1,6 @@
 
 exports.note = function (seq, rx) {
-  return seq === -1 ? -1 : seq << 1 | !rx
+  return seq === -1 ? -1 : seq === -2 ? -2 : seq << 1 | !rx
 }
 
 exports.getSequence = function getSequence(seq) {
@@ -8,17 +8,19 @@ exports.getSequence = function getSequence(seq) {
 }
 
 exports.getReplicate = function getReplicate(seq) {
-  return seq !== -1
+  return (seq || 0) > -1
 }
 
 exports.getReceive = function getReceive (seq) {
-  return !(seq & 1)
+  return (seq || 0) > -1 && !(seq & 1)
 }
 
-
-
-
-
+exports.isNotReplicate = function (seq) {
+  return seq === -1
+}
+exports.isForked = function (seq) {
+  return seq === -2
+}
 
 
 
